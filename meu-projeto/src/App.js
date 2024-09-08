85618f60a57e0bfc051270dee3cf79ea1e462010
dedlop <104914772+dedlop@users.noreply.github.com>
2024-09-08 18:22:58 -0300

    Props e Css Module

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
-  const name = 'Matheus'
-  const newName = name.toUpperCase()
-
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
+  const nome = 'Maria'
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha Imagem" />
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="André" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150" />
     </div>
     
   );
