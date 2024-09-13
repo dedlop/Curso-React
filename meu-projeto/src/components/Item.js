31b20a0870fe525b64578515a3a0b62dfcb9af77
dedlop <104914772+dedlop@users.noreply.github.com>
2024-09-12 21:21:22 -0300

    Passando eventos por props

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,24 +1,23 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-function Item(marca, ano_lancamento) {
+// Corrigindo a definição do componente
+function Item(props) {
+    const { marca, ano_lancamento } = props; // Extraindo propriedades do objeto props
     return (
-        <>
-            <li>
-                {marca} - {ano_lancamento}
-            </li>
-        </>
-    )
+        <li>
+            {marca} - {ano_lancamento}
+        </li>
+    );
 }
 
 Item.propTypes = {
     marca: PropTypes.string.isRequired,
     ano_lancamento: PropTypes.number,
-}
+};
 
 Item.defaultProps = {
     marca: 'Faltou a marca',
     ano_lancamento: 0,
-}
+};
 
-export default Item
-
+export default Item;
