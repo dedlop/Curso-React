29a4aec9ad1a6f3b226d1f61de0068bf9c0171d4
dedlop <104914772+dedlop@users.noreply.github.com>
2024-09-16 22:48:35 -0300

    Renderização de Listas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-import Evento from './components/Evento'
-import Form from './components/Form';
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+  const meusItens = ['React', 'Vue', 'Angular']
+
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento />
-      <Form />      
-    </div>
-    
-  );
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
+    </div>    
+  )
 }
 
 export default App;
